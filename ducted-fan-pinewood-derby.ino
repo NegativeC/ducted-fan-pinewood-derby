ae9d4a74eb150080535a57b2adbb7348726df103
Christian Hubbell <hubbellc@gmail.com>
2018-01-09 23:18:53 -0500

    code refactoring

--- ducted-fan-pinewood-derby.ino
+++ ducted-fan-pinewood-derby.ino
@@ -22,32 +22,29 @@ enum raceState {
   STAGED,
   RACING
 };
-
 raceState currentState = OFF;
 
-void setup()
-{
+void setup() {
+  // ESC
   pinMode(SERVOPIN, OUTPUT);                // Set up ESC control pin as an output
   esc.attach(SERVOPIN);                     // Attach the pin to the software servo library
-
+  // LED
   pinMode(LEDPIN, OUTPUT);                  // Set Trinket pin 1 as an output for LED
-
+  // IR BEAM
   pinMode(IRBEAMPIN, INPUT);                // Set up IR beam pin as input
   digitalWrite(IRBEAMPIN, HIGH);            // Enable then internal pull up resistor
-  
+  // Interupt Registers
   OCR0A = 0xAF;                             // Set up a 2ms interrupt
   TIMSK |= _BV(OCIE0A);
 }
 
-void loop()
-{
+void loop() {
   // Use the IR beam sensor to determine the current state of the race
   if (digitalRead(IRBEAMPIN) == HIGH)       // IR beam NOT broken
   {
-    if (currentState == STAGED)             // If car is currently staged
-    {
-      if ((millis() - stagedTime) > MINIMUMSTAGEDDURATION)  // If car has been staged for longer than the staged duration
-      {
+    if (currentState == STAGED) {           // If car is currently staged
+      // If car has been staged for longer than the staged duration
+      if ((millis() - stagedTime) > MINIMUMSTAGEDDURATION) {
         racingTime = millis();              // Start race duration timer
         currentState = RACING;              // ENTER RACE MODE
       }
@@ -55,33 +52,28 @@ void loop()
         currentState = OFF;                 // Off mode
     }
   }
-  else                                      // IR beam BROKEN
-  {
-    if (currentState == OFF)
-    {
+  else {                                    // IR beam BROKEN
+    if (currentState == OFF) {
       stagedTime = millis();                // Start "Staged" timer
       currentState = STAGED;                // CAR IS STAGED
     } 
   }
 
   // Turn off the motor after a specified race duration
-  if (currentState == RACING)               // If the car is currently racing
-  {
-    if ((millis() - racingTime) > MAXIMUMRACEDURATION)  // Measure the race duration and compare it to maximum race duration
-    {
+  if (currentState == RACING) {             // If the car is currently racing
+    // Measure the race duration and compare it to maximum race duration
+    if ((millis() - racingTime) > MAXIMUMRACEDURATION) {
       currentState = OFF;                   // Turn ESC off
     }
   }
   // Set the ESC speed depending on the current state
-  if (currentState == STAGED)
-  {
+  if (currentState == STAGED) {
     esc.write(STAGEDSPEED);
     digitalWrite(LEDPIN, HIGH);             // Turn ON LED
   }
   else if (currentState == RACING)
     esc.write(RACINGSPEED);
-  else
-  {
+  else {
     esc.write(OFFSPEED);
     digitalWrite(LEDPIN, LOW);              // Turn OFF LED
   }
@@ -89,11 +81,9 @@ void loop()
 }
 
 // The SIGNAL(TIMER0_COMPA_vect) function is the interrupt that will be called by the microcontroller every 2 milliseconds
-SIGNAL(TIMER0_COMPA_vect)
-{
+SIGNAL(TIMER0_COMPA_vect) {
   counter += 2;                             // Add two milliseconds to the elapsed time
-  if (counter >= 20)                        // Has 20ms elapsed?
-  {
+  if (counter >= 20) {                      // Has 20ms elapsed?
     counter = 0;                            // Reset the timer
     esc.refresh();                          // Refesh the software based servo control
   }
