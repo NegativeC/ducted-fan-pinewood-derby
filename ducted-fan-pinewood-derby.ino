a5b522ec55c18bef8d62050835d5099e1a688794
Daniel J. Rodriguez <37150651+DanGitR@users.noreply.github.com>
2021-01-25 23:40:23 -0600

    Add files via upload

--- ducted-fan-pinewood-derby.ino
+++ ducted-fan-pinewood-derby.ino
@@ -1,26 +1,38 @@
-#include <Adafruit_SoftServo.h>
+ /******************************************************************
+ Trinket:        USB+   Gnd   Pin #0  Pin #1  Pin #2 A1
+ Connection:     Servo+  -   Servo1 Servo2   Potentiometer wiper
+ 
+ *******************************************************************/
 
+#include <Adafruit_SoftServo.h> // SoftwareServo (works on non PWM pins)
+
+// We'll take advantage of the built in millis() timer that goes off
+// to keep track of time, and refresh the servo every 20 milliseconds
 volatile uint8_t counter = 0;
-unsigned long stagedTime = 0;
-unsigned long racingTime = 0;
+unsigned long stagedTime = 0; //Initialize timer when staged for race state is detected (IR or capacitive detection of lane pin).
+unsigned long racingTime = 0; //Initialize timer when staged for race state is detected (IR, capacitive, or accelerometer)
 
 Adafruit_SoftServo esc;
 
 #define SERVOPIN  0                         // Servo control output pin
 #define LEDPIN    1                         // LED output pin
 #define IRBEAMPIN 2                         // IR beam input pin
+//#define CAPCONTPIN TBD                    // Capacitive contact sensor input pin 
+//#define ACCXPIN TBD                       // X-axis accelerometer input pin
 
 #define OFFSPEED    0                       // Lowest ESC speed (range 0-180)
-#define STAGEDSPEED 25                      // ESC speed when staged before race start
+#define STAGEDSPEED 25                      // ESC speed when staged before race start (sdle speed to avoid start torque surge while racing)
 #define RACINGSPEED 180                     // Maximum ESC speed when race start detected 
 
 #define MINIMUMSTAGEDDURATION 2000          // Minimum duration car must be stage to enter race mode in milliseconds
-#define MAXIMUMRACEDURATION 1500            // Maximum duration of racing speed in milliseconds
+#define MAXIMUMRACEDURATION 2000            // Maximum duration of racing speed in milliseconds (changed from orginal 1500 ms)
 
 enum raceState {
   OFF,
   STAGED,
   RACING,
+  //ACCELERATING
+  //DECELERATING
   DONE                                      // Do not allow motor to turn back on after race until power reset
 };
 raceState currentState = OFF;
@@ -33,15 +45,20 @@ void setup() {
   pinMode(LEDPIN, OUTPUT);                  // Set Trinket pin 1 as an output for LED
   // IR BEAM
   pinMode(IRBEAMPIN, INPUT);                // Set up IR beam pin as input
-  digitalWrite(IRBEAMPIN, HIGH);            // Enable then internal pull up resistor
-  // Interupt Registers
-  OCR0A = 0xAF;                             // Set up a 2ms interrupt
-  TIMSK |= _BV(OCIE0A);
+  digitalWrite(IRBEAMPIN, HIGH);            // Enable the internal pull up resistor
+  //pinMode(CAPCONTPIN, INPUT);                // Set up the contact capacitor pin as input
+  //digitalWrite(CAPCONTPIN, HIGH);            // Enable the internal pull up resistor
+  
+  // Interrupt Registers
+     // Set up the interrupt that will refresh the servo for us automagically
+  OCR0A = 0xAF;                             // Set up a 2ms interrupt // any number is OK 
+  TIMSK |= _BV(OCIE0A);                     // Turn on the compare interrupt (below!)
 }
 
 void loop() {
   // Use the IR beam sensor to determine the current state of the race
   if (digitalRead(IRBEAMPIN) == HIGH)       // IR beam NOT broken
+  //if (digitalRead(CAPCONTPIN) == HIGH)       // No capacitance contact detected
   {
     if (currentState == STAGED) {           // If car is currently staged
       // If car has been staged for longer than the staged duration
@@ -81,7 +98,7 @@ void loop() {
   delay(1);
 }
 
-// The SIGNAL(TIMER0_COMPA_vect) function is the interrupt that will be called by the microcontroller every 2 milliseconds
+// The SIGNAL(TIMER0_COMPA_vect) function is the interrupt that will be called by the microcontroller every 2 milliseconds (See TrinkeKnob example).
 SIGNAL(TIMER0_COMPA_vect) {
   counter += 2;                             // Add two milliseconds to the elapsed time
   if (counter >= 20) {                      // Has 20ms elapsed?
